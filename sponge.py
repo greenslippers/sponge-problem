ddcdb7cdc57e15bac8a01352666872a3dcbf77ec
eugeniakashanskaya <jkashanskaya@gmail.com>
2025-05-20 15:44:42 -0700

    Solved

--- sponge.py
+++ sponge.py
@@ -1,6 +1,27 @@
 def sponge_case(sentence):
     # Write your solution here!
-    pass
+    # To solve the sponge case problem, the main idea is to:
+    # - Process each word individually,
+    # - Convert the first letter of each word to lowercase,
+    # - Then alternate the casing (uppercase/lowercase) for each following letter in that word.
+
+    words = sentence.split()
+    modified_words = []
+
+    for word in words:
+        modified_word = ""
+        index = 0
+        for char in word:
+            if index % 2 == 0:
+                modified_word += char.lower()
+            else:
+                modified_word += char.upper()
+            index +=1
+        modified_words.append(modified_word)
+    
+    return " ".join(modified_words)
+
+
 
 
 
